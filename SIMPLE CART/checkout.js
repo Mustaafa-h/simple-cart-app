74cd9ae75b9159732e1bed72a172af4e480aa447
Mustaafa-h <86521814+Mustaafa-h@users.noreply.github.com>
2024-08-13 22:56:52 +0300

    Add files via upload

---
+++ SIMPLE CART/checkout.js
@@ -0,0 +1,43 @@
+let listCart = [];
+function checkCart(){
+        var cookieValue = document.cookie
+        .split('; ')
+        .find(row => row.startsWith('listCart='));
+        if(cookieValue){
+            listCart = JSON.parse(cookieValue.split('=')[1]);
+        }
+}
+checkCart();
+addCartToHTML();
+function addCartToHTML(){
+    // clear data default
+    let listCartHTML = document.querySelector('.returnCart .prodincartlist');
+    listCartHTML.innerHTML = '';
+
+    let totalQuantityHTML = document.querySelector('.checktotalQuantity');
+    let totalPriceHTML = document.querySelector('.totalPrice');
+    let totalQuantity = 0;
+    let totalPrice = 0;
+    // if has product in Cart
+    if(listCart){
+        listCart.forEach(product => {
+            if(product){
+                let newCart = document.createElement('div');
+                newCart.classList.add('prodincartitem');
+                newCart.innerHTML = 
+                    `<img src="${product.image}">
+                    <div class="info">
+                        <div class="prodincartname">${product.name}</div>
+                        <div class="prodincartprice">$${product.price}/1 product</div>
+                    </div>
+                    <div class="prodincartquantity">${product.quantity}</div>
+                    <div class="returnPrice">$${product.price * product.quantity}</div>`;
+                listCartHTML.appendChild(newCart);
+                totalQuantity = totalQuantity + product.quantity;
+                totalPrice = totalPrice + (product.price * product.quantity);
+            }
+        })
+    }
+    totalQuantityHTML.innerText = totalQuantity;
+    totalPriceHTML.innerText = '$' + totalPrice;
+}
